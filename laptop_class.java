30aadba69ba148c7db71f6f75353e5bd8fedbe8b
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-21 19:34:08 +0200

    Create laptop_class.java

---
+++ laptop_class.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Laptop {
+    // properties -- attributes -- fields -- data
+    public String color;
+    public float size;
+    public String chargerType;
+    public int comfKeyboard1_10;
+
+    // functions
+    public void turnOn() {
+        System.out.println("laptop is turned on ...");
+    }
+    public void turnOff() {
+        System.out.println("laptop is turned off ...");
+    }    
+}

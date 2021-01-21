dab128afdee54704a29d254df960cd0307703b39
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-21 18:12:12 +0200

    Create hw_solution.java

---
+++ hw_solution.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+
+    static Scanner s = new Scanner(System.in);
+    static Random r = new Random();
+
+    public static void main(String[] args)  {
+        String fname = getStringFromUser("Please enter your first name:");
+        String lname = getStringFromUser("Please enter your last name:");
+
+        int[] arr = { 1,2,3,4};
+        System.out.println(isLonger(arr , 4));
+
+    }
+
+    private static String getStringFromUser(String msg) {
+        System.out.println(msg);
+        String result = s.nextLine();
+        return result;
+    }
+
+    private static int howManyDigits(int number) {
+        String number_as_string = String.valueOf(number);
+        return number_as_string.length();
+    }
+
+    private static boolean isLonger(int[] numbers, int len) {
+        return numbers.length > len;
+    }
+
+    private static boolean arrayEqual(int[] numbers1, int[] numbers2) {
+        if (numbers1.length != numbers2.length) {
+            return false;
+        }
+        for (int index = 0; index < numbers1.length; index++) {
+            if (numbers1[index] != numbers2[index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
